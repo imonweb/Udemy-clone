c70dd269faa7eae7915c130135c4065e51541d41
Imon <contact@imonweb.co.uk>
2024-07-08 10:46:15 +0100

    login and signup form

---
+++ app/controllers/Login.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*
+* Login class
+*/
+
+class Login extends Controller
+{
+  public function index()
+  {
+    
+    $data['title'] = 'Login';
+
+    $this->view('login', $data);
+  }
+ 
+
+}
+ 
---
+++ app/controllers/Signup.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*
+* Signup class
+*/
+
+class Signup extends Controller
+{
+  public function index()
+  {
+    
+    $data['title'] = 'Signup';
+
+    $this->view('signup', $data);
+  }
+ 
+
+}
+ 
