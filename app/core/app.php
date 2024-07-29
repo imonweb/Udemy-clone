f13c4fa3e4c0c3d47d7054e1da1a5f6362bbc669
Imon <contact@imonweb.co.uk>
2024-07-29 23:31:59 +0100

    logout

--- app/core/app.php
+++ app/core/app.php
@@ -4,6 +4,7 @@ class App
 {
   protected $controller = '_404';
   protected $method = 'index';
+  public static $page = '_404';
 
   function __construct()
   {
@@ -15,6 +16,7 @@ class App
    {
     require $filename;
     $this->controller = $arr[0];
+    self::$page = $arr[0];
     unset($arr[0]);
    } else {
     require '../app/controllers/' . $this->controller . '.php';
